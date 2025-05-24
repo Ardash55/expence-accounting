e30039280a8f34d65169a5e91b59cdf3ebc264e5
Ardash55 <ardash30042005g@gmail.com>
2025-05-24 21:43:55 +0300

    Добавлены сканнер и листы с категориями и бюджетами

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Account> accounts = new ArrayList<>();
+        ArrayList<Category> categories = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
         Account cash = new Account(200, "Наличные");
         Category food = new Category("Еда", 200);
     }
