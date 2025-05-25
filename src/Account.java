4c5cd34b18bd364c46293da15cce9bca09f34389
Ardash55 <ardash30042005g@gmail.com>
2025-05-25 13:38:10 +0300

    Добаавлены методы показа категорий и счетов

--- src/Account.java
+++ src/Account.java
@@ -2,7 +2,7 @@ public class Account {
     private int amount;
     private String name;
 
-    public Account(int amount, String name) {
+    public Account(String name, int amount) {
         this.amount = amount;
         this.name = name;
     }
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,11 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
+        Category food = new Category("Еда", 200);
+        categories.add(food);
+        Category food2 = new Category("Еда2", 200);
+        categories.add(food2);
+
         while (true) {
             System.out.println("1. Добавить  категорию трат");
             System.out.println("2. Добавить счет");
@@ -23,11 +28,17 @@ public class Main {
                     addAccount();
                     break;
                 case 3:
-
+                    minusMoney();
+                    break;
+                case 4:
+                    showCategories();
+                    break;
+                case 5:
+                    showAccounts();
+                    break;
             }
             break;
         }
-        Category food = new Category("Еда", 200);
     }
 
     public static void addCategory() {
@@ -47,13 +58,32 @@ public class Main {
          String name = sc.nextLine();
          System.out.println("Введите сумму на счету");
          int sum = sc.nextInt();
-         Account account = new Account(sum, name);
+         Account account = new Account(name, sum);
          accounts.add(account);
          System.out.println("Счет добавлен");
     }
 
-//    public static void minusMoney(int sum, Account account, Category category) {
-//        account.minusSum(sum);
-//        category.plusSpending(sum);
-//    }
+    public static void minusMoney() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Выберите категорию трат:");
+        showCategories();
+        int categoryNumber = sc.nextInt();
+
+    }
+
+    public static void showCategories() {
+        int id = 1;
+        for (Category category : categories) {
+            System.out.println(id + ". " + category.getName() + ": " + category.getSpending());
+            id++;
+        }
+    }
+
+    public static void showAccounts() {
+        int id = 1;
+        for (Account account : accounts) {
+            System.out.println(id + ". " + account.getName() + ": " + account.getAmount());
+            id++;
+        }
+    }
 }
